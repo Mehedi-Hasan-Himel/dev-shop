da54b816fd29e0a4563b18e57dcb89c1f0566028
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2025-11-22 14:06:15 +0600

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import iconCart from "../assets/images/iconCart.png";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleStatusTab } from "../stores/cart.jsx";
+import SoftzinoLogo from "../assets/images/softzino_logo.png";
+
+const Header = () => {
+  const [totalQuantity, setTotalQuantity] = useState(0);
+  const carts = useSelector((store) => store.cart.items);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    let total = 0;
+    carts.forEach((item) => (total += item.quantity));
+    setTotalQuantity(total);
+  }, [carts]);
+  const handleOpenTabCart = () => {
+    dispatch(toggleStatusTab());
+  };
+  return (
+    <header className="flex justify-between items-center mb-5">
+      <Link to="/" className="flex items-center gap-2 text-xl">
+        <img src={SoftzinoLogo} alt="Softzino Logo" className="w-12 h-auto" />
+        <span className="text-3xl ">Softzino DevShop</span>{" "}
+      </Link>
+      <div
+        className="w-10 h-10 bg-gray-100 rounded-full
+        flex justify-center items-center relative"
+        onClick={handleOpenTabCart}
+      >
+        <img src={iconCart} alt="" className="w-6" />
+        <span
+          className="absolute top-2/3 right-1/2 bg-red-500 text-white text-sm
+            w-5 h-5 rounded-full flex justify-center items-center"
+        >
+          {totalQuantity}
+        </span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
